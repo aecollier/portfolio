aecb76b98c8b5345890dd16db5a44c2ac74a2ed4
abigail-collier <abigail.collier@datacamp.com>
2022-06-22 10:19:30 -0400

    added newest versions of goodreads and chromedriver

--- goodreads/goodreads.py
+++ goodreads/goodreads.py
@@ -2,95 +2,113 @@ from selenium import webdriver
 import random
 from bs4 import BeautifulSoup
 import time
-import smtplib
-from email.message import EmailMessage
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-def pos_words():
-    '''This list of positive sentiment words was created by 
-        Minqing Hu and Bing Liu. "Mining and Summarizing Customer Reviews." 
-        Proceedings of the ACM SIGKDD International Conference on Knowledge 
-        Discovery and Data Mining (KDD-2004), Aug 22-25, 2004, Seattle, 
-        Washington, USA. They asked that this paper be cited.'''
+def soupify(browser):
+    '''helper function for converting page to beautiful soup object.'''
+    html = browser.page_source
+    soup = BeautifulSoup(html, features = 'lxml')
+    return soup
+
+def get_recent_read():
+    '''
+    Takes user input of a book title and scrapes the book description from Goodreads.
+    Returns a tuple of the title the user entered and the scraped description.
+    '''
+    book_title = input("Enter a book you've enjoyed recently, or hit enter for a random quote: ")
+    if not book_title:
+       return ''
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    browser.get('https://www.goodreads.com/quotes')
+    time.sleep(3)
+    search = browser.find_element(by=By.XPATH, value="//input[@name='q']")
+    search.send_keys(book_title) # search for the book title
+    browser.find_element(by=By.XPATH, value = "//button[@aria-label='Search']").click()
+    time.sleep(1)
+    browser.find_element(by=By.XPATH, value = "//a[@class='bookTitle']").click()
+    time.sleep(5)
+    soup = soupify(browser)
+    # get page contents and find description.
+    div = soup.find('div', id = 'description')
+    if div:
+        spans = div.find_all('span')
+        desc_text = spans[1].text
+    browser.close()
+    return (book_title.title(), desc_text)
+
+def get_word_from_desc(desc):
+    '''
+    Takes a book description as scraped by get_recent_read. Removes a portion of stopwords, sorts in descending order,
+    and returns the longest word from the description. Choosing to return the longest under the surface assumption 
+    that a longer word will be more interesting/relevant for later steps.
+    '''
+    # NLTK would be better for this purpose, but it's a hassle to install, so hard-coding a list of some stopwords instead. 
+    stop_words = [ 'we', 'her','of', 'she', 'he', 'stop', 'the', 'to', 'and', 'a', 'in', 'it', 'is', 'i', 'that', 'had', 'on', 'for', 'were', 'was','an','at']
+    desc_list = desc.split()
+    possible_words = [word.lower() for word in desc_list if word.lower() not in stop_words and word.lower().isalpha()]
+    # throw out duplicates and sort
+    possible_words = set(possible_words)
+    sorted_words = sorted(possible_words, key = len, reverse=True)
+    return sorted_words[0]
+
+def pick_random_word():
+    '''
+    This list of positive sentiment words was created by  Minqing Hu and Bing Liu. "Mining and Summarizing Customer Reviews." 
+    Proceedings of the ACM SIGKDD International Conference on Knowledge Discovery and Data Mining (KDD-2004), Aug 22-25, 2004, Seattle, 
+    Washington, USA. They asked that this paper be cited.
+
+    In the case that the user doesn't enter a book title, this function is called instead. 
+    It returns a random word from this list of positive sentiment words.  
+    '''
     word_list = []
     with open('positive_words.txt','r') as fp:
         for line in fp:
             word_list.append(line.strip())
-    return word_list[35:] #remove documentation at beginning of file
+    word_list = word_list[35:] #remove documentation at beginning of file
+    return random.choice(word_list)
 
-def pick_word():
-    browser = webdriver.Chrome(executable_path=r'C:/Users/abiga/Documents/AVC/chromedriver.exe')
+def pick_quote(random_word):
+    '''
+    Takes a word either generated at random or parsed from the user's book selection. 
+    Searches Goodreads Quotes for this word and returns the top quote that includes this word. 
+    '''
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     browser.get('https://www.goodreads.com/quotes')
     time.sleep(3)
-    search = browser.find_element_by_xpath("//input[@id='explore_search_query']")
-    random_word = random.choice(pos_words())
+    # find search elements
+    search = browser.find_element(by=By.XPATH, value="//input[@id='explore_search_query']")
     search.click()
     search.send_keys(random_word)
-    browser.find_element_by_xpath("//input[@name='commit']").click()
+    browser.find_element(by=By.XPATH, value = "//input[@name='commit']").click()
     time.sleep(4)
-    #random_word = 'jubilant'
-    #browser.get('https://www.goodreads.com/quotes/search?utf8=%E2%9C%93&q=jubilant&commit=Search')
-    html = browser.page_source
+    soup = soupify(browser)
     browser.close()
-    return random_word, html
-
-def pick_quote(html):
-    soup = BeautifulSoup(html, features = 'lxml')
     quotes = soup.find_all('div', class_='quoteText')
-    random_int = random.randint(0,19)
-    random_quote = quotes[random_int]
-    author = random_quote.find('span', class_='authorOrTitle')
-    work = random_quote.find('a', class_='authorOrTitle')
-    quote_extras = random_quote.text.strip().split('―')
-    quote = quote_extras[0]+'\n ―'
-    if author:
-        quote += ' '+author.text.strip()
-    if work:
-        quote += ' '+work.text.strip()
+    random_quote = quotes[0]
+    quote = random_quote.text.strip()
     return(quote)
 
-def get_recent_read():
-    #title = input("Enter a book you've enjoyed recently, or hit enter for a random quote: ")
-    #if not title:
-    #    return ''
-    title = 'dune'
-    browser = webdriver.Chrome(executable_path=r'C:/Users/abiga/Documents/AVC/chromedriver.exe')
-    browser.get('https://www.amazon.com/')
-    time.sleep(3)
-    # first set search parameters to books because that's what we're after
-    filter_books = browser.find_element_by_xpath("//select[@id='searchDropdownBox']")
-    filter_books.click()
-    browser.find_element_by_xpath("//option[@value='search-alias=stripbooks']").click()
-    time.sleep(3)
-
-    searchbar = browser.find_element_by_xpath("//input[@id='twotabsearchtextbox']")
-    searchbar.send_keys(title.strip())
-    browser.find_element_by_xpath("//input[@id='nav-search-submit-button']").click()
-    time.sleep(5)
-    browser.find_element_by_xpath("//div[@cel_widget_id='MAIN-SEARCH_RESULTS-2']").click()
-    time.sleep(5)
-    browser.close()
-
-    
-
-
-
 def main():
-    get_recent_read()
-    # if not user_selection:
-    #     # if get_recent_read() returns an empty string, go ahead with generating a random quote
-    #     random_word, html = pick_word()
-    #     quote = pick_quote(html)
-    #     print(f"Today's word is {random_word}.")
-    #     print()
-    #     print("And today's quote is:\n")
-    #     print(quote)
+    user_selection = get_recent_read() # includes tuple of title and extracted description
+    if not user_selection:
+        # if get_recent_read() returns an empty string, go ahead with generating a random quote
+        random_word = pick_random_word()
+        quote = pick_quote(random_word)
+        print(f"Your random word is {random_word}.")
+        print()
+        print(f"And the quote generated by {random_word} is:\n")
+        print(quote)
     
-    # else:
+    else:
+        random_word = get_word_from_desc(user_selection[1])
+        quote = pick_quote(random_word)
+        print(f"The word selected from your latest read {user_selection[0]} is {random_word}.")
+        print()
+        print(f"And the quote generated by {random_word} is:\n")
+        print(quote)
 
-
-    # print("Your word was: "+random_word)
-    # print()
-    # print("Your quote was: ")
-    # print(pick_quote(html))
 if __name__ == "__main__":
     main()
